6d8ba2171de139c018c8de96ccb3a3995837738e
Hamlet <hamlet.villa@gmail.com>
2022-03-16 12:31:39 -0400

    user model rows added, signup component comment

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -14,6 +15,8 @@ class User(db.Model, UserMixin):
     profile_img = db.Column(db.Text)
     about = db.Column(db.Text)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
 
     posts = db.relationship('Post', back_populates='user', cascade='all, delete')
     comments = db.relationship('Comment', back_populates='user', cascade='all, delete')
