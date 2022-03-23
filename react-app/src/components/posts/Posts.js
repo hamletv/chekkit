f370ac50378e771031843f0da4cef407acb1ecf3
Hamlet <hamlet.villa@gmail.com>
2022-03-23 18:14:01 -0400

    posts styling

--- react-app/src/components/posts/Posts.js
+++ react-app/src/components/posts/Posts.js
@@ -10,32 +10,59 @@ const AllPosts = () => {
     const comments = useSelector(state => Object.values(state.comment))
     const user_id = useSelector(state => state.session.user?.id)
     const user = useSelector(state => state.session.user?.username)
-    console.log('THE POSTS ', posts, user);
+    console.log('THE POSTS ', user);
     return (
         <div className="all-posts-container">
             {posts?.map(post => (
                 <div className="post-container">
-                    <div className="vote-container">
-                        <div><i class="fa-thin fa-square-arrow-up"></i></div>
-                        <div>--</div>
-                        <div><i class="fa-thin fa-square-arrow-down"></i></div>
-                    </div>
-                    <Link to={`/posts/${post.id}`} key={post.id}>
-                        <div className="post">
-                            <div className="user-content">
-                            <p>Posted by: </p>
-                            </div>
+                    <div></div>
+                        <div className="vote-container">
+                            <button className="vote-button"></button>
+                                <span className="post-span"><i className="fa-thin fa-square-arrow-up"></i></span>
+                             <div className="num-vote"></div> {/* vote number here */}
+                            <button className="vote-button">
+                                <span className="post-span">
+                                <i class="fa-thin fa-square-arrow-down"></i>
+                                </span>
+                            </button>
                         </div>
-                        <div>
-                            <h3 className="post-title">{post?.title}</h3>
+                        <div className="content">
+                            <Link to={`/posts/${post.id}`} key={post.id}>
+                                <div className="post">
+                                    <div className="user-content">
+                                        <div className="user-image">
+                                        <a className="user-image-1"></a>
+                                        </div>
+                                    </div>
+                                    <div className="posted-by">
+                                        <div className="posted-bar">
+                                            <span className="posted-span">Posted by</span>
+                                            <div className="username">
+                                                <a className="user-link">{user}</a>
+                                                <a className="posted-time">{post?.created_at}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="title-container">
+                                    <div className="title-line">
+                                        <div className="post-title">
+                                            <h3 className="title">{post?.title}
+                                            </h3>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div className="image-container">
+                                    <div className="image-container-2">
+                                    <img className='image' src={post?.img_url} />
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-content">{post?.description}</p>
+                                </div>
+                            </Link>
                         </div>
-                        <div>
-                            <p className="text-content">{post?.description}</p>
-                        </div>
-                        <div>
-                            <img src={post?.img_url} />
-                        </div>
-                    </Link>
+
                 </div>
             ))}
         </div>
@@ -43,3 +70,37 @@ const AllPosts = () => {
 }
 
 export default AllPosts;
+
+
+
+
+
+// return (
+//     <div className="all-posts-container">
+//         {posts?.map(post => (
+//             <div className="post-container">
+//                 <div className="vote-container">
+//                     <div><i class="fa-thin fa-square-arrow-up"></i></div>
+//                     <div>--</div>
+//                     <div><i class="fa-thin fa-square-arrow-down"></i></div>
+//                 </div>
+//                 <Link to={`/posts/${post.id}`} key={post.id}>
+//                     <div className="post">
+//                         <div className="user-content">
+//                         <p>Posted by: </p>
+//                         </div>
+//                     </div>
+//                     <div>
+//                         <h3 className="post-title">{post?.title}</h3>
+//                     </div>
+//                     <div>
+//                         <p className="text-content">{post?.description}</p>
+//                     </div>
+//                     <div>
+//                         <img src={post?.img_url} />
+//                     </div>
+//                 </Link>
+//             </div>
+//         ))}
+//     </div>
+//  );
