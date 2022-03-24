d35401ba54c37b1b5231264bd1ed7640476fc1fd
Hamlet <hamlet.villa@gmail.com>
2022-03-24 00:14:31 -0400

    image format validation function added

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -6,6 +6,17 @@ from app.forms.post_form import PostForm
 post_routes = Blueprint('post_routes', __name__)
 
 
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+
 @post_routes.route('/')
 def all_posts():
     posts = Post.query.all()
--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -3,11 +3,17 @@ from wtforms import SubmitField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired, Email, ValidationError, Length, EqualTo, URL
 
 
+def valid_image_format(form, field):
+    # Check file format
+    img_url = field.data
+    if not img_url.endswith(('.png', '.jpg', '.jpeg', '.gif', '.tiff')):
+        raise ValidationError('File url must end with .png, .jpg, .jpeg, .gif or .tiff')
+
 
 class PostForm(FlaskForm):
     user_id = IntegerField('user_id')
     # votes = IntegerField('votes')
-    img_url = TextAreaField('img_url', validators=[DataRequired(message='Enter a valid image url'), URL(require_tld=True, message='The url provided is not valid.')])
+    img_url = TextAreaField('img_url', validators=[DataRequired(message='Enter a valid image url'), URL(require_tld=True, message='The url provided is not valid.'), valid_image_format])
     title = TextAreaField('title', validators=[DataRequired(message='Please enter a descriptive title.'), Length(min=5, max=300)])
     description = TextAreaField('description')
     submit = SubmitField('Post')
