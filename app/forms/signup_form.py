dbfb03a83b2f8f99fe3a669ba884c676e7c7793b
Hamlet <hamlet.villa@gmail.com>
2022-03-23 23:07:53 -0400

    added form fields, validators

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -21,10 +21,10 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired(message='Please enter your first name.')])
-    last_name = StringField('Last Name', validators=[DataRequired('Please enter your last name.')])
+    first_name = StringField('First Name', validators=[DataRequired(message='Please enter your first name.'), Length(min=2, max=60)])
+    last_name = StringField('Last Name', validators=[DataRequired('Please enter your last name.'), Length(min=2, max=60)])
     username = StringField(
         'Username', validators=[DataRequired(message='Please enter a username.'), username_exists, Length(min=2, max=25)])
-    email = StringField('Email', validators=[DataRequired(), user_exists, Email(message='Please provide a valid email')])
-    password = StringField('Password', validators=[DataRequired(message='Enter a password with at least 6 characters.'), Length(min=6, max=25)])
+    email = StringField('Email', validators=[DataRequired(), user_exists, Email(message='Please provide a valid email'), Length(min=5, max=50, message='Email must be > 5 and < 50 chars.')])
+    password = StringField('Password', validators=[DataRequired(message='Enter password with at least 6 characters.'), Length(min=6, max=25), EqualTo('confirm_password', message='Passwords must match'), Length(min=5, max=25, message='Password must be > 5 and < 20 chars.')])
     confirm_password = StringField('Confirm Password', validators=[DataRequired(message='Enter previously typed password'), EqualTo(password, message='Passwords must match.')])
