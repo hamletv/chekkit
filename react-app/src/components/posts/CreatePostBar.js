0285b1a4fa4d2691acd0c4ecbb416c4d41ed8492
Hamlet <hamlet.villa@gmail.com>
2022-03-30 00:14:44 -0400

    create post bar and styling done

--- react-app/src/components/posts/CreatePostBar.js
+++ react-app/src/components/posts/CreatePostBar.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 
 const CreatePostBar = () => {
     const user = useSelector(state => state.session.user);
-    console.log('THE USER: ', user);
+    const history = useHistory();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        history.push('/new');
+    }
 
     return (
-        <div className="_2jJN">
-            <a className="_2E_z" href="#">
-                <div className="efd">
-                    <div className="_1cy">
-                        <div className="_2_Qq"></div>
-                        <div className="_1XJ">
-                            <img className="Scrr" src={user?.profile_img}/>
+        <div className="cpb-container">
+            <a className="cpb-img-container-1" href="#">
+                <div className="cpb-img-container-2">
+                    <div className="cpb-img-container-3">
+                        <div className="cpb-img-container-4"></div>
+                        <div className="cpb-img-container-5">
+                            <img className="cpb-image" src={user?.profile_img}/>
                         </div>
                     </div>
                 </div>
             </a>
-            <input className="zgT" placeholder="Create Post"/>
-            <a className="_27e" href="#"><i className="fa-solid fa-image"></i></a>
-            <a className="_27e" href="#"><i className="fa-solid fa-link"></i></a>
+            <input className="cpb-input" placeholder="Create Post" onClick={handleClick}/>
+            <a className="cpb-icons" href="#"><i className="fa-solid fa-image"></i></a>
+            <a className="cpb-icons" href="#"><i className="fa-solid fa-link"></i></a>
         </div>
     );
 }
